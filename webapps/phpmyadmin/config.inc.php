9bdf2865f7e63bafd60c26996253679733230a19
Stefan Auditor <stefan.auditor@erdfisch.de>
2016-04-13 19:14:33 +0200

    saving uncommitted changes in /etc prior to pacman run

--- webapps/phpmyadmin/config.inc.php
+++ webapps/phpmyadmin/config.inc.php
@@ -31,7 +31,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /**
  * phpMyAdmin configuration storage settings.
